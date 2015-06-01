f7b55cee0eca822034fd2619194e9201c6e07fa9
Jan Winkler <winkler@cs.uni-bremen.de>
2015-06-01 10:41:16 +0200

    Reasoning on map objects working

--- shopping_scenario_reasoning/prolog/init.pl
+++ shopping_scenario_reasoning/prolog/init.pl
@@ -36,3 +36,10 @@
 :- use_module(library('shopping_scenario_reasoning')).
 
 :- owl_parser:owl_parse('package://shopping_scenario_models/owl/shopping_area.owl').
+
+
+:- register_ros_package(iai_semantic_maps).
+:- owl_parser:owl_parse('package://shopping_scenario_models/owl/shopping_area.owl').
+:- owl_parser:owl_parse('package://iai_semantic_maps/owl/iai_maps_addons.owl').
+
+:- rdf_db:rdf_register_ns(knowrob, 'http://ias.cs.tum.edu/kb/knowrob.owl#', [keep(true)]).
