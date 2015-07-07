be4d77c58c14b7d02053a88ae88a22faa8ff5c93
Jan Winkler <winkler@cs.uni-bremen.de>
2015-07-07 10:51:25 +0200

    Fixed manipulation paddings

--- shopping_scenario_executive/src/costmap-metadata.lisp
+++ shopping_scenario_executive/src/costmap-metadata.lisp
@@ -33,7 +33,7 @@
   (<- (costmap-origin -5 -5))
   (<- (costmap-resolution 0.05))
   
-  (<- (costmap-padding 0.5))
-  (<- (costmap-manipulation-padding 0.45))
-  (<- (costmap-in-reach-distance 0.4))
-  (<- (costmap-reach-minimal-distance 0.45)))
+  (<- (costmap-padding 0.4))
+  (<- (costmap-manipulation-padding 0.3))
+  (<- (costmap-in-reach-distance 0.3))
+  (<- (costmap-reach-minimal-distance 0.2)))
